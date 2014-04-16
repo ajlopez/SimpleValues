677b50ccd503a4a93e448db61946d8e3b1a9864e
ajlopez <webmaster@ajlopez.com>
2014-04-16 09:43:19 -0300

    Array first tests

---
+++ test/array.js
@@ -0,0 +1,33 @@
+
+var sv = require('..');
+
+exports['create empty array'] = function (test) {
+    var array = sv.array();
+    
+    test.ok(array);
+    test.equal(typeof array, 'function');
+    test.strictEqual(array.count(), 0);
+};
+
+exports['create array with initial data'] = function (test) {
+    var array = sv.array([1,2,3]);
+    
+    test.ok(array);
+    test.equal(typeof array, 'function');
+    test.strictEqual(array.count(), 3);
+    test.strictEqual(array(0), 1);
+    test.strictEqual(array(1), 2);
+    test.strictEqual(array(2), 3);
+};
+
+exports['create array, set and get value'] = function (test) {
+    var array = sv.array();
+    
+    test.ok(array);
+    test.equal(typeof array, 'function');
+    test.strictEqual(array.count(), 0);
+    test.strictEqual(array(1, 2), 2);
+    test.strictEqual(array(1), 2);
+    test.strictEqual(array.count(), 1);
+};
+
