4d601ed22a8bf3a270d979f562d20e11684391c9
ajlopez <webmaster@ajlopez.com>
2014-04-17 17:34:10 -0300

    Array push

--- test/array.js
+++ test/array.js
@@ -31,3 +31,18 @@ exports['create array, set and get value'] = function (test) {
     test.strictEqual(array.count(), 1);
 };
 
+exports['push new data'] = function (test) {
+    var array = sv.array();
+    
+    test.ok(array);
+    test.equal(typeof array, 'function');
+    test.strictEqual(array.count(), 0);
+    array.push(1);
+    array.push(2);
+    array.push(4);
+    test.strictEqual(array.count(), 3);
+    test.strictEqual(array(0), 1);
+    test.strictEqual(array(1), 2);
+    test.strictEqual(array(2), 4);
+};
+
