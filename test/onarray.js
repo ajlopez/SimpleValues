da3e25a1e69e8f15aa45c5339fd10226ec315bb8
ajlopez <webmaster@ajlopez.com>
2014-04-18 07:58:47 -0300

    Array events, first tests

---
+++ test/onarray.js
@@ -0,0 +1,47 @@
+
+var sv = require('..');
+
+exports['on set'] = function (test) {
+    test.async();
+    
+    var array = sv.array();
+    
+    array.on('set', function (data, key) {
+        test.ok(data);
+        test.strictEqual(data, 'foo');
+        test.strictEqual(key, 0);
+        test.done();
+    });
+    
+    array(0, 'foo');
+};
+
+exports['on set same value'] = function (test) {
+    test.async();
+    
+    var array = sv.array(['foo']);
+    
+    array.on('set', function (data, key) {
+        test.ok(data);
+        test.strictEqual(data, 'foo');
+        test.strictEqual(key, 0);
+        test.done();
+    });
+    
+    array(0, 'foo');
+};
+
+exports['on change'] = function (test) {
+    test.async();
+    
+    var array = sv.array();
+    
+    array.on('change', function (data, key) {
+        test.ok(data);
+        test.strictEqual(data, 'foo');
+        test.strictEqual(key, 0);
+        test.done();
+    });
+    
+    array(0, 'foo');
+};
